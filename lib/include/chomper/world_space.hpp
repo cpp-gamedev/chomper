247f5b96c1178e8a9fc3a37807de26170a8eb308
StanPlayzz <118762037+stanplayzz@users.noreply.github.com>
2026-01-11 22:55:28 +0100

    Add collisions (#22)

--- lib/include/chomper/world_space.hpp
+++ lib/include/chomper/world_space.hpp
@@ -1,10 +1,11 @@
 #pragma once
 #include "chomper/world_size.hpp"
+#include "glm/common.hpp"
 #include <glm/vec2.hpp>
 
 namespace chomper::worldSpace {
 namespace {
-constexpr auto halfGridSize = worldSize_v * 0.5f;
+auto const halfGridSize = glm::ceil(worldSize_v * 0.5f);
 // only add half the tilesize when that axis is even
 constexpr auto tileOffset =
 	glm::vec2{(static_cast<int>(worldSize_v.x) % 2 == 0) ? tileSize_v.x * 0.5f : 0.0f, (static_cast<int>(worldSize_v.y) % 2 == 0) ? tileSize_v.y * 0.5f : 0.0f};
@@ -15,5 +16,9 @@ constexpr auto gridToWorld(glm::vec2 gridPosition) {
 }
 constexpr auto worldToGrid(glm::vec2 worldPosition) {
 	return glm::floor((worldPosition - tileOffset) / tileSize_v) + halfGridSize;
+}
+
+constexpr auto isOutOfBounds(glm::vec2 gridPoint) {
+	return gridPoint.x <= 0 || gridPoint.y <= 0 || gridPoint.x > worldSize_v.x || gridPoint.y > worldSize_v.y;
 }
 } // namespace chomper::worldSpace
--- lib/src/player.cpp
+++ lib/src/player.cpp
@@ -1,14 +1,77 @@
 #include "chomper/player.hpp"
 #include "chomper/controllers/player_controller.hpp"
+#include "chomper/engine.hpp"
+#include "chomper/runtimes/entrypoint.hpp"
+#include "chomper/world_space.hpp"
+#include <algorithm>
 
 namespace chomper {
-Player::Player(le::input::ScopedActionMapping& mapping) {
+namespace {
+constexpr auto moveSpeed_v = kvf::Seconds{0.135f};
+constexpr auto oppositeHeading_v = klib::EnumArray<Heading, Heading>{Heading::West, Heading::South, Heading::East, Heading::North};
+constexpr auto headingToDir_v = klib::EnumArray<Heading, glm::vec2>{glm::vec2{1.f, 0.f}, glm::vec2{0.f, 1.f}, glm::vec2{-1.f, 0.f}, glm::vec2{0.f, -1.f}};
+} // namespace
+
+Player::Player(le::input::ScopedActionMapping& mapping, gsl::not_null<Engine*> engine) : m_engine(engine) {
 	createController(mapping);
 }
 
 void Player::tick(kvf::Seconds dt) {
 	m_controller->tick(dt);
-	m_snake.tick(dt);
+
+	m_moveTimer += dt;
+
+	if (m_moveTimer >= moveSpeed_v) {
+		m_moveTimer = {};
+		move();
+	}
+}
+
+bool Player::isCollidingWithSelf(glm::vec2 const targetGrid) const {
+	if (m_snake.getSegments().empty()) {
+		return false;
+	}
+	return std::ranges::any_of(m_snake.getSegments(), [targetGrid](le::RenderInstance const& s) {
+		return worldSpace::worldToGrid(s.transform.position) == targetGrid;
+	});
+}
+
+bool Player::isCollidingWithWall(glm::vec2 const targetGrid) const {
+	if (m_snake.getSegments().empty()) {
+		return false;
+	}
+	return worldSpace::isOutOfBounds(targetGrid);
+}
+
+void Player::move() {
+	// no body, no movement
+	if (m_snake.getSegments().empty()) {
+		return;
+	}
+
+	if (!m_headingQueue.empty()) {
+		m_heading = m_headingQueue.front();
+		m_headingQueue.erase(m_headingQueue.begin());
+	}
+
+	auto const targetGrid = worldSpace::worldToGrid(m_snake.getSegments().back().transform.position) + headingToDir_v[m_heading];
+	if (isCollidingWithSelf(targetGrid) || isCollidingWithWall(targetGrid)) {
+		if (m_graceMove) {
+			m_engine->setNextRuntime<runtime::Entrypoint>();
+		} else {
+			m_graceMove = true;
+		}
+
+		return; // return so you don't move when you collide
+	}
+
+	m_snake.grow(m_heading);
+
+	if (m_shouldPop) {
+		m_snake.popTail();
+	}
+
+	m_graceMove = false;
 }
 
 void Player::draw(le::IRenderer& renderer) const {
@@ -16,7 +79,14 @@ void Player::draw(le::IRenderer& renderer) const {
 }
 
 void Player::debugInspect() {
-	m_snake.debugInspect();
+	ImGui::TextUnformatted(klib::FixedString{"Heading: {}", headingName_v[m_heading]}.c_str());
+
+	if (ImGui::TreeNode("heading queue")) {
+		for (auto const heading : m_headingQueue) {
+			ImGui::TextUnformatted(headingName_v[heading].data());
+		}
+		ImGui::TreePop();
+	}
 }
 
 void Player::createController(le::input::ScopedActionMapping& mapping) {
@@ -24,7 +94,15 @@ void Player::createController(le::input::ScopedActionMapping& mapping) {
 }
 
 void Player::onSetHeading(Heading const heading) {
-	m_snake.setHeading(heading);
+	auto lastHeading = m_headingQueue.empty() ? m_heading : m_headingQueue.back();
+	if (heading == m_heading || heading == oppositeHeading_v[lastHeading]) {
+		return;
+	}
+
+	if (m_headingQueue.size() < 3) {
+		m_log.debug("changing heading from {} to {}", headingName_v[m_heading], headingName_v[heading]);
+		m_headingQueue.push_back(heading);
+	}
 }
 
 } // namespace chomper
