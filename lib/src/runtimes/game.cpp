54b8c0e2cf4c8ef513ff416c12595ecd57b48ae6
= <stanbremer056@gmail.com>
2026-01-17 19:59:07 +0100

    Added `Collectible`s
    Added `Collectible`s and the ability to eat a `Collectible`

--- lib/src/runtimes/game.cpp
+++ lib/src/runtimes/game.cpp
@@ -1,7 +1,16 @@
 #include "chomper/runtimes/game.hpp"
+#include "chomper/collectible.hpp"
 #include "chomper/im_util.hpp"
 #include "chomper/runtimes/entrypoint.hpp"
+#include "chomper/world_size.hpp"
+#include "chomper/world_space.hpp"
+#include "glm/ext/vector_float2.hpp"
+#include "le2d/resource/texture.hpp"
 #include <array>
+#include <memory>
+#include <random>
+#include <unordered_set>
+#include <vector>
 
 namespace chomper::runtime {
 using ActionValue = le::input::action::Value;
@@ -9,10 +18,14 @@ using ActionValue = le::input::action::Value;
 Game::Game(gsl::not_null<Engine*> engine) : m_engine(engine), m_mapping(&engine->getInputRouter()) {
 	createPlayer();
 	m_world = std::make_unique<World>(m_engine);
+	createCollectibleTexture();
+	spawnCollectible();
 }
 
 void Game::tick(kvf::Seconds const dt) {
 	m_player->tick(dt);
+
+	collideCollectibles();
 
 	ImGui::SetNextWindowSize({300.0f, 300.0f}, ImGuiCond_Once);
 	if (ImGui::Begin("Debug Inspect")) {
@@ -29,6 +42,9 @@ void Game::tick(kvf::Seconds const dt) {
 void Game::render(le::IRenderer& renderer) const {
 	m_world->draw(renderer);
 	m_player->draw(renderer);
+	for (auto const& collectible : m_collectibles) {
+		collectible.draw(renderer);
+	}
 }
 
 void Game::debugInspectWindow() {
@@ -57,6 +73,60 @@ void Game::createPlayer() {
 	bindActions();
 	// create the player, passing a reference of the logger and a reference of the input mapping to create its PlayerController.
 	m_player = std::make_unique<Player>(m_mapping, m_engine);
+}
+
+void Game::createCollectibleTexture() {
+	m_collectibleTexture = m_engine->getResources().load<le::ITexture>("images/apple.png");
+}
+
+void Game::spawnCollectible() {
+	std::unordered_set<int> occupied;
+	for (auto const& seg : m_player->getSegments()) {
+		auto p = worldSpace::worldToGrid(seg.transform.position);
+		occupied.insert(static_cast<int>((p.y * worldSize_v.x) + p.x));
+	}
+
+	for (auto const& c : m_collectibles) {
+		auto p = c.getGridPosition();
+		occupied.insert(static_cast<int>((p.y * worldSize_v.x) + p.x));
+	}
+
+	auto emptyTiles = (worldSize_v.x * worldSize_v.y) - (float)occupied.size();
+	if (emptyTiles <= 0) {
+		return;
+	}
+
+	std::mt19937 rng(std::random_device{}());
+	std::uniform_int_distribution<int> dist(0, (int)emptyTiles - 1);
+	auto target = dist(rng);
+
+	auto count = 0;
+	for (float y = 0; y < worldSize_v.y; ++y) {
+		for (float x = 0; x < worldSize_v.x; ++x) {
+			auto id = static_cast<int>((y * worldSize_v.x) + x);
+			if (!occupied.contains(id)) {
+				if (count == target) {
+					m_collectibles.emplace_back(*m_collectibleTexture, worldSpace::gridToWorld({x, y}));
+					return;
+				}
+				count++;
+			}
+		}
+	}
+}
+
+void Game::collideCollectibles() {
+	for (auto it = m_collectibles.begin(); it != m_collectibles.end();) {
+		if (it->getGridPosition() == worldSpace::worldToGrid(m_player->getSegments().back().transform.position)) {
+
+			m_collectibles.erase(it);	// erase collided collectible
+			m_player->shouldPop(false); // tell the player to not pop the tail/grow
+			spawnCollectible();			// spawn new collectible
+
+			return;
+		}
+		it++;
+	}
 }
 
 void Game::onGoBack() {
